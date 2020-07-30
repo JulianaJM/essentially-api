c20576a1f236b5b2e8dc00c5b376ceac846d8f23
Juliana <juliana.jeanmarie972@gmail.com>
2020-07-30 13:28:15 +0200

    handle cache

--- app.js
+++ app.js
@@ -33,14 +33,22 @@ app.use(bodyParser.urlencoded({ extended: false }));
 // app.get("/random", function(req, res) {
 // });
 
+// handle cache 
+app.set('etag', false)
+
 app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
+
+  // handle cache 
+  res.set('Cache-Control', 'no-store')
+  
   next();
 });
 
 app.use("/api", routes);
+
 
 // error handler
 app.use((err, req, res, next) => {
